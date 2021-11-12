5108eb2e307d76140b35ae6e3e1f369f10a08e6a
Anjali Saidanavaru <anjalisaidanavaru1@gmail.com>
2021-11-12 07:21:59 -0800

    initial commit

---
+++ app/src/main/java/com/example/gmap_route/asd.java
@@ -0,0 +1,18 @@
+package com.example.gmap_route;
+
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class asd extends AppCompatActivity implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
+
+}
